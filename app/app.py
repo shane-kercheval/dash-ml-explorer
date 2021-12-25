de77c2ac1f95d7e11f26df8c32e11b8211e005b7
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-12-24 18:13:35 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,177 +1,112 @@
+import os
+import urllib.parse
+
 import dash
 import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.Button("Sidebar", outline=True, color="secondary", className="mr-1", id="btn_sidebar"),
-        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("More pages", header=True),
-                dbc.DropdownMenuItem("Page 2", href="#"),
-                dbc.DropdownMenuItem("Page 3", href="#"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
-    ],
-    brand="Brand",
-    brand_href="#",
-    color="dark",
-    dark=True,
-    fluid=True,
-)
-
-
-# the style arguments for the sidebar. We use position:fixed and a fixed width
+# styling the sidebar
 SIDEBAR_STYLE = {
     "position": "fixed",
-    "top": 62.5,
+    "top": 0,
     "left": 0,
     "bottom": 0,
     "width": "16rem",
-    "height": "100%",
-    "z-index": 1,
-    "overflow-x": "hidden",
-    "transition": "all 0.5s",
-    "padding": "0.5rem 1rem",
+    "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
 
-SIDEBAR_HIDEN = {
-    "position": "fixed",
-    "top": 62.5,
-    "left": "-16rem",
-    "bottom": 0,
-    "width": "16rem",
-    "height": "100%",
-    "z-index": 1,
-    "overflow-x": "hidden",
-    "transition": "all 0.5s",
-    "padding": "0rem 0rem",
-    "background-color": "#f8f9fa",
-}
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
+# padding for the page content
 CONTENT_STYLE = {
-    "transition": "margin-left .5s",
     "margin-left": "18rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
 }
 
-CONTENT_STYLE1 = {
-    "transition": "margin-left .5s",
-    "margin-left": "2rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
+PROJECT_DIRECTORY = '../projects'
+PROJECT_DIRECTORY_ = PROJECT_DIRECTORY + '/'
+
+project_names = [name for name in os.listdir(PROJECT_DIRECTORY) if os.path.isdir(PROJECT_DIRECTORY_ + name)]
+project_names.sort()
+project_dropdowns = [{'label': x, 'value': x} for x in project_names]
 
 sidebar = html.Div(
     [
-        html.H2("Sidebar", className="display-4"),
+        html.H2("ML Explorer", className="display-6"),
         html.Hr(),
         html.P(
-            "A simple sidebar layout with navigation links", className="lead"
+            "Projects", className="lead"
+        ),
+        dcc.Dropdown(
+            id='project_names_dropdown',
+            options=project_dropdowns,
+            value=project_dropdowns[0]['value']
+        ),
+        html.Hr(),
+        html.P(
+            "Models", className="lead"
         ),
         dbc.Nav(
-            [
-                dbc.NavLink("Page 1", href="/page-1", id="page-1-link"),
-                dbc.NavLink("Page 2", href="/page-2", id="page-2-link"),
-                dbc.NavLink("Page 3", href="/page-3", id="page-3-link"),
-            ],
+            id='project_links',
             vertical=True,
             pills=True,
         ),
     ],
-    id="sidebar",
     style=SIDEBAR_STYLE,
 )
 
-content = html.Div(
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
 
-    id="page-content",
-    style=CONTENT_STYLE)
-
-app.layout = html.Div(
-    [
-        dcc.Store(id='side_click'),
-        dcc.Location(id="url"),
-        navbar,
-        sidebar,
-        content,
-    ],
-)
+app.layout = html.Div([
+    dcc.Location(id="url"),
+    sidebar,
+    content
+])
 
 
 @app.callback(
-    [
-        Output("sidebar", "style"),
-        Output("page-content", "style"),
-        Output("side_click", "data"),
-    ],
-
-    [Input("btn_sidebar", "n_clicks")],
-    [
-        State("side_click", "data"),
+    Output('project_links', 'children'),
+    Input('project_names_dropdown', 'value')
+)
+def update_project_links(project_names_dropdown_value):
+    run_file_names = [name for name in os.listdir(PROJECT_DIRECTORY_ + project_names_dropdown_value)
+                      if os.path.isdir(PROJECT_DIRECTORY_ + project_names_dropdown_value + '/' + name)]
+    run_file_names.sort()
+    project_links_children = [
+        dbc.NavLink(x,
+                    href=urllib.parse.quote('/' + project_names_dropdown_value + '/' + x),
+                    active='exact')
+        for x in run_file_names
     ]
-)
-def toggle_sidebar(n, nclick):
-    if n:
-        if nclick == "SHOW":
-            sidebar_style = SIDEBAR_HIDEN
-            content_style = CONTENT_STYLE1
-            cur_nclick = "HIDDEN"
-        else:
-            sidebar_style = SIDEBAR_STYLE
-            content_style = CONTENT_STYLE
-            cur_nclick = "SHOW"
-    else:
-        sidebar_style = SIDEBAR_STYLE
-        content_style = CONTENT_STYLE
-        cur_nclick = 'SHOW'
 
-    return sidebar_style, content_style, cur_nclick
+    return project_links_children
 
-# this callback uses the current pathname to set the active state of the
-# corresponding nav link to true, allowing users to tell see page they are on
+
 @app.callback(
-    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
-    [Input("url", "pathname")],
+    Output("page-content", "children"),
+    [Input("url", "pathname")]
 )
-def toggle_active_links(pathname):
-    if pathname == "/":
-        # Treat page 1 as the homepage / index
-        return True, False, False
-    return [pathname == f"/page-{i}" for i in range(1, 4)]
-
-
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
-    if pathname in ["/", "/page-1"]:
-        return html.P("This is the content of page 1!")
-    elif pathname == "/page-2":
-        return html.P("This is the content of page 2. Yay!")
-    elif pathname == "/page-3":
-        return html.P("Oh cool, this is page 3!")
-    # If the user tries to reach a different page, return a 404 message
-    return dbc.Jumbotron(
-        [
+    # if pathname == "/":
+    #     return [
+    #             html.H1('asdf',
+    #                     style={'textAlign':'center'}),
+    #             dcc.Graph(id='asdf',
+    #                      figure=px.bar(df, barmode='group', x='asdf',
+    #                      y=['asdf', 'asdf']))
+    #             ]
+    # elif pathname == "/page-1":
+
+    return [
             html.H1("404: Not found", className="text-danger"),
             html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
+            html.P(f"The pathname `{pathname}` was not found.")
         ]
-    )
 
 
-if __name__ == "__main__":
-    app.run_server(debug=True, port=8086)
+if __name__ == '__main__':
+    app.run_server(debug=True, port=3000)
